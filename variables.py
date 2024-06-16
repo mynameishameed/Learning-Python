b562c12d11c7f8b82558d5e617ebeeb30898a80a
mynameishameed <abdulhameedkhan646.ahk@gmail.com>
2024-06-16 14:36:00 +0530

    variable in python

---
+++ variables.py
@@ -0,0 +1,10 @@
+#lets us know about the variables in python
+#basically in python a variable means a container that store a value it in, or you can say that
+#  a value is assigned
+# let's say you want ask the user for input like what is your name?
+print (input("what is your name? "))  #here you can see the name is printed
+#but each time you watn to prkint the name you have to write the whole line of code
+#now if you assign the input to a variable called name
+name = input("what is your name? ")  
+#and now you are just printig the name
+print(name)
